37943b252845ecd31220810e05008e04a2055215
pythagoras <magicianhy@126.com>
2019-01-05 21:52:12 +0800

    change

--- src/main/java/org/py/frames/DesktopDemo.java
+++ src/main/java/org/py/frames/DesktopDemo.java
@@ -30,7 +30,6 @@ public class DesktopDemo {
         JFrame frame = new JFrame("DesktopDemo");
         frame.setContentPane(new DesktopDemo().mPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.pack();
         frame.setSize(500, 200);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
